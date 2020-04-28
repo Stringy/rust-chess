518934c378be486c42dce8a9fda16fc6b967a499
Giles Hutton <hutton.giles@gmail.com>
2020-04-28 06:53:29 +0100

    Added more masks for diagonals and rank/files

--- src/masks/mod.rs
+++ src/masks/mod.rs
@@ -1,4 +1,19 @@
-pub mod diagonals;
+pub mod a1h8;
+pub mod a8h1;
+pub mod files;
 pub mod pawns;
+pub mod ranks;
 pub mod sliding;
 pub mod special;
+
+#[rustfmt::skip]
+pub const RANK_SHIFT: [usize; 64] = [
+    1, 1, 1, 1, 1, 1, 1, 1,
+    9, 9, 9, 9, 9, 9, 9, 9,
+    17, 17, 17, 17, 17, 17, 17, 17,
+    25, 25, 25, 25, 25, 25, 25, 25,
+    33, 33, 33, 33, 33, 33, 33, 33,
+    41, 41, 41, 41, 41, 41, 41, 41,
+    49, 49, 49, 49, 49, 49, 49, 49,
+    57, 57, 57, 57, 57, 57, 57, 57,
+];
